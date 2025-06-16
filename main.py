e6ff13ef189b7de7a9e85fc0a4bc02d02bdb44b7
Paolo Paci <paolopci@yahoo.it>
2025-06-16 17:46:52 +0200

    my solution company page

--- main.py
+++ main.py
@@ -41,3 +41,8 @@ with col4:
         st.write(row["description"])
         st.image('images/'+row["image"])
         st.write(f"[Source Code]({row['url']})")
+
+
+lorem = """
+lorem80 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
