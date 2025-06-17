f28b6014b51fd2de449952adb0bbedbdaa3898e2
Paolo Paci <paolopci@yahoo.it>
2025-06-17 17:01:26 +0200

    file pdf multi pages creato da topic.csv

--- app-pdf.py
+++ app-pdf.py
@@ -1,17 +1,26 @@
 from fpdf import FPDF
+import pandas as pd
+# This is a simple example of how to create a PDF file using the FPDF library in Python.
+
+# leggo il file topics.csv
+df = pd.read_csv('topics.csv')
 
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
-pdf.add_page()
-# Set font for the first line
-pdf.set_font("Arial", size=12, style='B')
-# Add a cell with the first line ln=1 --> to move to the next line
-# w=0 --> to use the full width of the page, h=12 --> height of the cell
-pdf.cell(w=0, h=12, txt='Hello world!', align='L', ln=1, border=1)
-# Set font for the second line,style='I' --> italic
-pdf.set_font("Times", size=10, style='I')
-pdf.cell(0, 12, 'Hello world!', align='L', ln=1, border=1)
+
+for index, row in df.iterrows():
+    # Add a new page for each topic
+    icount = 0
+    while icount < int(row['Pages']):
+        pdf.add_page()
+        # Set font for the topic title
+        pdf.set_font("Arial", size=16, style='B')
+        pdf.set_text_color(254, 0, 0)  # Red color for the title
+        # Add a cell with the topic title
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='L', ln=1)
+        pdf.line(10, 20, 200, 20)  # Draw a line under the title
+        icount += 1
 
 
 # generate a pdf
