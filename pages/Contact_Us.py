9663ebbe2956a6a88eec1ea4469c28b87a25a540
Paolo Paci <paolopci@yahoo.it>
2025-06-17 14:40:33 +0200

    add subject input

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -7,8 +7,10 @@ st.header("Contact Me")
 with st.form(key='email_forms'):
     user_email = st.text_input(
         "Your email address", placeholder="Enter your email")
+    email_subject = st.text_input(
+        "Subject", placeholder="Enter email subject")
     user_message = st.text_area(
         "Your message", height=200, placeholder="Enter your message here")
     submit_button = st.form_submit_button("Submit")
 if submit_button:
-    send_email(user_message, user_email)
+    send_email(user_message, email_subject, user_email)
--- send_email2.py
+++ send_email2.py
@@ -4,7 +4,7 @@ import smtplib
 
 
 # def send_email(sender, password, receiver, subject, body):
-def send_email(message, user_email):
+def send_email(message, subject, user_email):
     host = "smtp.gmail.com"
     port = 465
     sender = "paolopci@gmail.com"
@@ -14,7 +14,7 @@ def send_email(message, user_email):
     em = EmailMessage()
     em['From'] = sender
     em['To'] = user_email
-    em['Subject'] = "Oggetto della mail"
+    em['Subject'] = subject
     em.set_content(message)
 
     context = ssl.create_default_context()
