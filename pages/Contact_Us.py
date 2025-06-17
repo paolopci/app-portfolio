ef1b9e133bc85db024d68661aa5167af76056670
Paolo Paci <paolopci@yahoo.it>
2025-06-17 10:55:07 +0200

    initial create Contact Us page

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 
 st.header("Contact Me")
+
+with st.form(key='email_forms'):
+    user_email = st.text_input(
+        "Your email address", placeholder="Enter your email")
+    user_message = st.text_area(
+        "Your message", height=200, placeholder="Enter your message here")
+    submit_button = st.form_submit_button("Submit")
+    if submit_button:
+        pass
