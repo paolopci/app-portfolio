28c0c59a8069e6ddad8a8e7d58ff341fdc4f111a
Paolo Paci <paolopci@yahoo.it>
2025-06-18 15:54:53 +0200

    complete project

--- app-animals.py
+++ app-animals.py
@@ -10,25 +10,24 @@ class PDF(FPDF):
 
 
 file_paths = glob.glob('animals/*.txt')
+
 pdf = PDF(orientation="P", unit="mm", format="A4")
 
 for index, file in enumerate(file_paths, start=1):
+    print(file)
     filename = Path(file).stem.capitalize()
     pdf.add_page()
     pdf.set_font("Arial", size=15)
     pdf.cell(w=100, h=8, txt=f"{filename}", ln=1)
+    try:
+        with open(file, "r") as f:
+            content = f.read()
+
+        pdf.set_font("Arial", size=10)
+        pdf.multi_cell(w=0, h=8, txt=content)
+    except Exception as e:
+        print(f"Error reading file {file}: {str(e)}")
+
 
 output_name = os.path.join("animals/pdf/", f"animals.pdf")
 pdf.output(output_name)
-
-#  output_name = os.path.join("animals/pdf/", f"{filename}.pdf")
-#     pdf.output(output_name)
-# try:
-#     with open(file, 'r') as f:
-#         content = f.read()
-#         print(f"File {index}: {content}")
-#         print('')
-#         print('# ----------------------------------------------')
-#         print('')
-# except Exception as e:
-#     print(f"Error reading file {file}: {str(e)}")
