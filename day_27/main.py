50e7447f96e4b3050fd6b4ca55b60f81fbd612c7
Paolo Paci <paolopci@yahoo.it>
2025-06-19 12:39:09 +0200

    invio un email con tutti gli articoli 20 !!!!

--- day_27/main.py
+++ day_27/main.py
@@ -3,7 +3,11 @@ from send_email2 import send_email
 import requests
 
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-05-19&sortBy=publishedAt&apiKey=2f4bc007f9094545b72fb6a4a37be75d"
+url = "https://newsapi.org/v2/everything?q=tesla&" \
+    "from=2025-05-19&" \
+    "sortBy=publishedAt&" \
+    "apiKey=2f4bc007f9094545b72fb6a4a37be75d&" \
+    "language=en"  # solo quelli in lingua inglese
 
 
 headers = {
@@ -14,15 +18,16 @@ headers = {
 req = requests.get(url, headers=headers)
 content = req.json()
 
-
+body = ''
 icount = 0
-for item in content['articles']:
-    # print(item['title'])
-    # print(item['description'])
-    message = item['description']
-    oggetto = item['title']
-    destinatario = "paolopci@libero.it"
-    if icount < 5:
-        send_email(message, oggetto, "paolopci@libero.it")
-        icount += 1
-        print(f"{icount} - email invia a : {destinatario}")
+for item in content['articles'][:20]:
+    description = item.get(
+        'description') or "Nessuna descrizione disponibile."+"\n"
+    body += description+"\n" + \
+        item.get('url') or "Nessun URL disponibile."+2*"\n"
+
+
+message = body
+oggetto = item['title']
+destinatario = "paolopci@libero.it"
+send_email(message, oggetto, "paolopci@libero.it")
