1f0e10495dba15d7dac08ba1ff463f52859d27a4
Paolo Paci <paolopci@yahoo.it>
2025-06-23 10:13:24 +0200

    step 2: initial dynamic plot

--- day_32/main.py
+++ day_32/main.py
@@ -12,3 +12,17 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+
+def get_data(days):
+    # grafico
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = [10, 11, 15]
+    temperatures = [days*i for i in temperatures]
+    return dates, temperatures
+
+
+figure = px.line(x=dates, y=temperatures, labels={
+                 "x": "Date", "y": "Tempersture (C)"})
+
+st.plotly_chart(figure)
