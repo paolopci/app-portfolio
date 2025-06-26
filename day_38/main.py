258e834929f37d6d3dc9c985f7e01a2b7e403dda
Paolo Paci <paolopci@yahoo.it>
2025-06-26 17:22:29 +0200

    start example from main.py

--- day_38/main.py
+++ day_38/main.py
@@ -1,6 +1,15 @@
-import requests
-import selectorlib
+# import requests
+# import selectorlib
 import service as sc
 
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
+
+
+scraped = sc.scrape(URL)
+extracted = sc.extract(scraped)
+print(extracted)
+
+if extracted != "No upcoming tours" and sc.is_new(extracted):
+    sc.store(extracted)
+    sc.send_email(extracted)
