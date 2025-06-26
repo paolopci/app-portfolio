cd7cb2731a6986492e87d05052d59ed002dfbf5b
Paolo Paci <paolopci@yahoo.it>
2025-06-26 17:28:17 +0200

    Create loop While True with time.sleep(1)
    in questa maniera controllo la pagina web ogni secondo -- è solo un test!!!!!!

--- day_38/main.py
+++ day_38/main.py
@@ -1,15 +1,19 @@
 # import requests
 # import selectorlib
 import service as sc
+import time
 
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 
-scraped = sc.scrape(URL)
-extracted = sc.extract(scraped)
-print(extracted)
+while True:
 
-if extracted != "No upcoming tours" and sc.is_new(extracted):
-    sc.store(extracted)
-    sc.send_email(extracted)
+    scraped = sc.scrape(URL)
+    extracted = sc.extract(scraped)
+    print(extracted)
+
+    if extracted != "No upcoming tours" and sc.is_new(extracted):
+        sc.store(extracted)
+        sc.send_email(extracted)
+    time.sleep(1)
