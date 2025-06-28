e17fdc98ba7d83ee20fe63b215e0b33147081de7
Paolo Paci <paolopci@yahoo.it>
2025-06-28 12:43:46 +0200

    fix

--- app-hotel/main.py
+++ app-hotel/main.py
@@ -1,18 +1,35 @@
 import pandas as pd
 
 
-df = pd.read_csv("hotels.csv")
+df = pd.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        # sovrascrive il file CSV in modo che lo stato aggiornato
+        #  resti valido fra esecuzioni diverse del programma.
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """
+            df.loc[df["id"] == self.hotel_id, "available"]
+            Si sta selezionando la colonna available solo per le righe in cui 
+            la colonna id è uguale a self.hotel_id.
+        """
+        availability = df.loc[df["id"] ==
+                              self.hotel_id, "available"]  # filtro su id
+        print(availability)
+        # Accedere all’elemento con availability.iloc[0] per fare un
+        # confronto esplicito con "yes"
+        if not availability.empty and availability.iloc[0] == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationTicket:
@@ -24,8 +41,8 @@ class ReservationTicket:
 
 
 # main program -------------------------------
-id = input("Enter the id of the hotel: ")
-hotel = Hotel(id)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
