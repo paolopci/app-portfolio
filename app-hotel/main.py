c4f22c7a20e94c639e2bcbacca5b9b1eb07207a3
Paolo Paci <paolopci@yahoo.it>
2025-06-28 19:44:52 +0200

    add CreditCard class

--- app-hotel/main.py
+++ app-hotel/main.py
@@ -2,9 +2,24 @@ import pandas as pd
 
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+
+
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
 
 
 class Hotel:
+
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
         self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
@@ -19,7 +34,7 @@ class Hotel:
     def available(self):
         """
             df.loc[df["id"] == self.hotel_id, "available"]
-            Si sta selezionando la colonna available solo per le righe in cui 
+            Si sta selezionando la colonna available solo per le righe in cui
             la colonna id è uguale a self.hotel_id.
         """
         availability = df.loc[df["id"] ==
@@ -34,6 +49,7 @@ class Hotel:
 
 
 class ReservationTicket:
+
     def __init__(self, customer_name, hotel_object):
         self.customer_name = customer_name
         self.hotel = hotel_object
@@ -48,14 +64,16 @@ class ReservationTicket:
         return content
 
 
-# main program -------------------------------
+        # main program -------------------------------
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(
-        customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
-else:
-    print("Hotel is not free.")
+    credit_card = CreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(
+            customer_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("Hotel is not free.")
